ddbd4fb3b703ebdaf04a42671a02ecb687337df2
kristinacho <155475770+kristinacho@users.noreply.github.com>
2025-06-27 14:49:56 +0500

    Update container.cpp

--- Task5/container.cpp
+++ Task5/container.cpp
@@ -1,4 +1,5 @@
-/* Продолжение задания №5 Контейнер.
+/*
+ * Продолжение задания №5 Контейнер.
  * Элементами контейнеров являются целые числа. Для
  * заполнения контейнера использовать итератор и конструктор соответствующего контейнера,
  * для вывода элементов использовать итератор (для вывода элементов в обратном порядке
@@ -9,474 +10,243 @@
  * Даны дек D и список L. Каждый исходный контейнер содержит не менее 5
  * элементов. Вставить перед пятым с конца элементом списка последние 5 элементов дека в
  * обратном порядке. Использовать один вызов функции-члена insert.
- */
+*/
 
 #include "container.h"
-#include <iostream> 
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-class Deque {
-  private:
-    struct Node {
-        int value;
-        Node* prev;
-        Node* next;
-        Node(int val) : value(val), prev(nullptr), next(nullptr) {}
-    };
+Deque::Deque() : front(nullptr), back(nullptr), size(0) {}
 
-    Node* front;
-    Node* back;
-    int size; 
-
-  public:
-    Deque() : front(nullptr), back(nullptr), size(0) {}
-    ~Deque() {
-        clear();
-    }
-
-    void pushBack(int value) {
-        Node* newNode = new Node(value);
-        if (isEmpty()) {
-            front = back = newNode;
-        } else {
-            back->next = newNode;
-            newNode->prev = back;
-            back = newNode;
-        }
-        size++;
-    }
-
-    void pushFront(int value) {
-        Node* newNode = new Node(value);
-        if (isEmpty()) {
-            front = back = newNode;
-        } else {
-            newNode->next = front;
-            front->prev = newNode;
-            front = newNode;
-        }
-        size++;
-    }
-
-    int popBack() {
-        if (isEmpty()) {
-            cout << "Ошибка: Дек пуст, невозможно удалить элемент из конца." << endl;
-            return 0;
-        }
-        int value = back->value;
-        Node* temp = back;
-        if (front == back) {
-            front = back = nullptr;
-        } else {
-            back = back->prev;
-            back->next = nullptr;
-        }
-        delete temp;
-        size--;
-        return value;
-    }
-
-    int popFront() {
-        if (isEmpty()) {
-            cout << "Ошибка: Дек пуст, невозможно удалить элемент из начала." << endl;
-            return 0;
-        }
-        int value = front->value;
-        Node* temp = front;
-        if (front == back) {
-            front = back = nullptr;
-        } else {
-            front = front->next;
-            front->prev = nullptr;
-        }
-        delete temp;
-        size--;
-        return value;
-    }
-
-    int getBack(int index) const {
-        if (index < 0 || index >= size) {
-            cout << "Ошибка: Индекс " << index << " вне допустимого диапазона для дека (размер " << size << ")." << endl;
-            return 0;
-        }
-        Node* current = back;
-        for (int i = 0; i < index; i++) {
-            current = current->prev;
-        }
-        return current->value;
-    }
-
-    int getSize() const {
-        return size;
-    }
-
-    bool isEmpty() const {
-        return size == 0;
-    }
-
-    void clear() {
-        while (!isEmpty()) {
-            popFront();
-        }
-    }
-
-void display() const {
-        Node* current = front;
-        while (current != nullptr) {
-            cout << current->value << " ";
-            current = current->next;
-        }
-        cout << endl;
-    }
-
-void displayReverse() const {
-        Node* current = back;
-        while (current != nullptr) {
-            cout << current->value << " ";
-            current = current->prev;
-        }
-        cout << endl;
-    }
-};
-
-
-DequePtr CreateDeque() {
-    return new Deque();
+Deque::~Deque() {
+    clear();
 }
 
-void DestroyDeque(DequePtr dq) {
-    if (dq) {
-        delete dq;
-    }
-}
-
-void PushBackDeque(DequePtr dq, int value) {
-    if (dq) {
-        dq->pushBack(value);
+void Deque::pushBack(int value) {
+    Node* newNode = new Node(value);
+    if (isEmpty()) {
+        front = back = newNode;
     } else {
-        cout << "Ошибка: Указатель на дек недействителен для pushBack." << endl;
+        back->next = newNode;
+        newNode->prev = back;
+        back = newNode;
     }
+    size++;
 }
 
-void PushFrontDeque(DequePtr dq, int value) {
-    if (dq) {
-        dq->pushFront(value);
+void Deque::pushFront(int value) {
+    Node* newNode = new Node(value);
+    if (isEmpty()) {
+        front = back = newNode;
     } else {
-        cout << "Ошибка: Указатель на дек недействителен для pushFront." << endl;
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
     }
+    size++;
 }
 
-int PopBackDeque(DequePtr dq) {
-    if (!dq) {
-        cout << "Ошибка: Указатель на дек недействителен для popBack." << endl;
-        return 0;
-    }
-    return dq->popBack();
-}
+int Deque::popBack() {
+    if (isEmpty()) throw runtime_error("Deque is empty");
 
-int PopFrontDeque(DequePtr dq) {
-    if (!dq) {
-        cout << "Ошибка: Указатель на дек недействителен для popFront." << endl;
-        return 0;
-    }
-    return dq->popFront();
-}
+    int value = back->value;
+    Node* temp = back;
 
-int GetBackDeque(const DequePtr dq, int index) {
-    if (!dq) {
-        cout << "Ошибка: Указатель на дек недействителен для GetBackDeque." << endl;
-        return 0;
-    }
-    return dq->getBack(index);
-}
-
-int GetDequeSize(const DequePtr dq) {
-    if (!dq) {
-        cout << "Предупреждение: Указатель на дек недействителен для GetDequeSize, возвращен размер 0." << endl;
-        return 0;
-    }
-    return dq->getSize();
-}
-
-bool IsDequeEmpty(const DequePtr dq) {
-    if (!dq) {
-        cout << "Предупреждение: Указатель на дек недействителен для IsDequeEmpty, считается пустым." << endl;
-        return true;
-    }
-    return dq->isEmpty();
-}
-
-void ClearDeque(DequePtr dq) {
-    if (dq) {
-        dq->clear();
+    if (front == back) {
+        front = back = nullptr;
     } else {
-        cout << "Ошибка: Указатель на дек недействителен для ClearDeque." << endl;
+        back = back->prev;
+        back->next = nullptr;
     }
+
+    delete temp;
+    size--;
+    return value;
 }
 
-void DisplayDeque(const DequePtr dq) {
-    if (dq) {
-        dq->display();
+int Deque::popFront() {
+    if (isEmpty()) throw runtime_error("Deque is empty");
+
+    int value = front->value;
+    Node* temp = front;
+
+    if (front == back) {
+        front = back = nullptr;
     } else {
-        cout << "Ошибка: Указатель на дек недействителен для DisplayDeque." << endl;
+        front = front->next;
+        front->prev = nullptr;
+    }
+
+    delete temp;
+    size--;
+    return value;
+}
+
+int Deque::getBack(int index) const {
+    if (index < 0 || index >= size) throw out_of_range("Index out of range");
+
+    Node* current = back;
+    for (int i = 0; i < index; i++) {
+        current = current->prev;
+    }
+    return current->value;
+}
+
+int Deque::getSize() const {
+    return size;
+}
+
+bool Deque::isEmpty() const {
+    return size == 0;
+}
+
+void Deque::clear() {
+    while (!isEmpty()) {
+        popBack();
     }
 }
 
-void DisplayDequeReverse(const DequePtr dq) {
-    if (dq) {
-        dq->displayReverse();
+void Deque::display() const {
+    Node* current = front;
+    while (current != nullptr) {
+        cout << current->value << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+void Deque::displayReverse() const {
+    Node* current = back;
+    while (current != nullptr) {
+        cout << current->value << " ";
+        current = current->prev;
+    }
+    cout << endl;
+}
+
+List::List() : head(nullptr), tail(nullptr), size(0) {}
+
+List::~List() {
+    clear();
+}
+
+void List::pushBack(int value) {
+    Node* newNode = new Node(value);
+    if (isEmpty()) {
+        head = tail = newNode;
     } else {
-        cout << "Ошибка: Указатель на дек недействителен для DisplayDequeReverse." << endl;
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
+    size++;
 }
 
-
-class List {
-  private:
-    struct Node {
-        int value;
-        Node* prev;
-        Node* next;
-        Node(int val) : value(val), prev(nullptr), next(nullptr) {}
-    };
-
-    Node* head;
-    Node* tail;
-    int size;
-
-  public:
-    List() : head(nullptr), tail(nullptr), size(0) {}
-    ~List() {
-        clear();
-    }
-
-void pushBack(int value) {
-        Node* newNode = new Node(value);
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-        size++;
-    }
-
-    void pushFront(int value) {
-        Node* newNode = new Node(value);
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-        size++;
-    }
-
-    int popBack() {
-        if (isEmpty()) {
-            cout << "Ошибка: Список пуст, невозможно удалить элемент из конца." << endl;
-            return 0;
-        }
-        int value = tail->value;
-        Node* temp = tail;
-        if (head == tail) {
-            head = tail = nullptr;
-        } else {
-            tail = tail->prev;
-            tail->next = nullptr;
-        }
-        delete temp;
-        size--;
-        return value;
-    }
-
-    int popFront() {
-        if (isEmpty()) {
-            cout << "Ошибка: Список пуст, невозможно удалить элемент из начала." << endl;
-            return 0;
-        }
-        int value = head->value;
-        Node* temp = head;
-        if (head == tail) { 
-            head = tail = nullptr;
-        } else {
-            head = head->next;
-            head->prev = nullptr;
-        }
-        delete temp;
-        size--;
-        return value;
-    }
-
-    bool isEmpty() const {
-        return size == 0;
-    }
-
-    int getSize() const {
-        return size;
-    }
-
-    void clear() {
-        while (!isEmpty()) {
-            popFront();
-        }
-    }
-
-    void display() const {
-        Node* current = head;
-        while (current != nullptr) {
-            cout << current->value << " ";
-            current = current->next;
-        }
-        cout << endl;
-    }
-
-    void displayReverse() const {
-        Node* current = tail;
-        while (current != nullptr) {
-            cout << current->value << " ";
-            current = current->prev;
-        }
-        cout << endl;
-    }
-
-    void insertNodeBefore(Node* newNode, Node* targetNode) {
-        if (!targetNode) {
-            if (head == nullptr) {
-                head = tail = newNode;
-            } else {
-                tail->next = newNode;
-                newNode->prev = tail;
-                tail = newNode;
-            }
-        } else {
-            newNode->next = targetNode;
-            newNode->prev = targetNode->prev;
-
-            if (targetNode->prev != nullptr) {
-                targetNode->prev->next = newNode;
-            } else {
-                head = newNode; // Вставка в начало списка.
-            }
-            targetNode->prev = newNode;
-        }
-        size++;
-    }
-};
-
-
-ListPtr CreateList() {
-    return new List();
-}
-
-void DestroyList(ListPtr lst) {
-    if (lst) {
-        delete lst;
-    }
-}
-
-void PushBackList(ListPtr lst, int value) {
-    if (lst) {
-        lst->pushBack(value);
+void List::pushFront(int value) {
+    Node* newNode = new Node(value);
+    if (isEmpty()) {
+        head = tail = newNode;
     } else {
-        cout << "Ошибка: Указатель на список недействителен для PushBackList." << endl;
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
     }
+    size++;
 }
 
-void PushFrontList(ListPtr lst, int value) {
-    if (lst) {
-        lst->pushFront(value);
+int List::popBack() {
+    if (isEmpty()) throw runtime_error("List is empty");
+
+    int value = tail->value;
+    Node* temp = tail;
+
+    if (head == tail) {
+        head = tail = nullptr;
     } else {
-        cout << "Ошибка: Указатель на список недействителен для PushFrontList." << endl;
+        tail = tail->prev;
+        tail->next = nullptr;
     }
+
+    delete temp;
+    size--;
+    return value;
 }
 
-int PopBackList(ListPtr lst) {
-    if (!lst) {
-        cout << "Ошибка: Указатель на список недействителен для PopBackList." << endl;
-        return 0;
-    }
-    return lst->popBack();
-}
+int List::popFront() {
+    if (isEmpty()) throw runtime_error("List is empty");
 
-int PopFrontList(ListPtr lst) {
-    if (!lst) {
-        cout << "Ошибка: Указатель на список недействителен для PopFrontList." << endl;
-        return 0;
-    }
-    return lst->popFront();
-}
+    int value = head->value;
+    Node* temp = head;
 
-bool IsListEmpty(const ListPtr lst) {
-    if (!lst) {
-        cout << "Предупреждение: Указатель на список недействителен для IsListEmpty, считается пустым." << endl;
-        return true;
-    }
-    return lst->isEmpty();
-}
-
-int GetListSize(const ListPtr lst) {
-    if (!lst) {
-        cout << "Предупреждение: Указатель на список недействителен для GetListSize, возвращен размер 0." << endl;
-        return 0;
-    }
-    return lst->getSize();
-}
-
-void ClearList(ListPtr lst) {
-    if (lst) {
-        lst->clear();
+    if (head == tail) {
+        head = tail = nullptr;
     } else {
-        cout << "Ошибка: Указатель на список недействителен для ClearList." << endl;
+        head = head->next;
+        head->prev = nullptr;
+    }
+
+    delete temp;
+    size--;
+    return value;
+}
+
+bool List::isEmpty() const {
+    return size == 0;
+}
+
+int List::getSize() const {
+    return size;
+}
+
+void List::clear() {
+    while (!isEmpty()) {
+        popBack();
     }
 }
 
-void DisplayList(const ListPtr lst) {
-    if (lst) {
-        lst->display();
-    } else {
-        cout << "Ошибка: Указатель на список недействителен для DisplayList." << endl;
+void List::display() const {
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->value << " ";
+        current = current->next;
     }
+    cout << endl;
 }
 
-void DisplayListReverse(const ListPtr lst) {
-    if (lst) {
-        lst->displayReverse();
-    } else {
-        cout << "Ошибка: Указатель на список недействителен для DisplayListReverse." << endl;
+void List::displayReverse() const {
+    Node* current = tail;
+    while (current != nullptr) {
+        cout << current->value << " ";
+        current = current->prev;
     }
+    cout << endl;
 }
 
-bool InsertBeforeFifthFromEnd(ListPtr lst, const DequePtr dq) {
-    if (!lst) {
-        cout << "Ошибка: Указатель на список недействителен для InsertBeforeFifthFromEnd." << endl;
-        return false;
-    }
-    if (!dq) {
-        cout << "Ошибка: Указатель на дек недействителен для InsertBeforeFifthFromEnd." << endl;
-        return false;
-    }
+void List::insertBeforeFifthFromEnd(const Deque& deque) {
+    if (size < 5) throw runtime_error("List size must be at least 5");
+    if (deque.getSize() < 5) throw runtime_error("Deque size must be at least 5");
 
-    if (lst->getSize() < 5) {
-        cout << "Ошибка: Размер списка (" << lst->getSize() << ") должен быть не менее 5 для выполнения вставки." << endl;
-        return false;
-    }
-    if (dq->getSize() < 5) {
-        cout << "Ошибка: Размер дека (" << dq->getSize() << ") должен быть не менее 5 для предоставления элементов." << endl;
-        return false;
-    }
-
-    List::Node* insertPos = lst->tail;
+  
+    Node* current = tail;
     for (int i = 0; i < 4; i++) {
-        insertPos = insertPos->prev;
+        current = current->prev;
     }
+
 
     for (int i = 0; i < 5; i++) {
-        List::Node* newNode = new List::Node(dq->getBack(i));
-        lst->insertNodeBefore(newNode, insertPos);
+        Node* newNode = new Node(deque.getBack(i));
+        newNode->next = current;
+        newNode->prev = current->prev;
+
+        if (current->prev != nullptr) {
+            current->prev->next = newNode;
+        } else {
+            head = newNode;
+        }
+
+        current->prev = newNode;
+        size++;
     }
-    return true;
 }
